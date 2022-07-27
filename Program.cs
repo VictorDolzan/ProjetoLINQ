6ca4dba4007d244f6c71910fb0f5877018dd8c7e
Victor Dolsan <victorddf5@gmail.com>
2022-07-27 19:54:42 -0300

    Manipulando LINQ com Where, Select, Contains, orderBy

--- Program.cs
+++ Program.cs
@@ -7,11 +7,40 @@ namespace ProjetoLINQ
     {
         static void Main(string[] args)
         {
-            int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            // int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            // int[] numeros2 = {1, 7, 8, 9, 2, 3, 4, 5, 6, 10};
 
-            foreach (var n in numeros.Where(x => x > 4))
+            // var resultado = from num in numeros
+            //                 where num > 4
+            //                 select num;
+
+            // var resultado2 = from num in numeros2   
+            //                  where num > 4
+            //                  orderby num
+            //                  select num; 
+
+            // var resultadoOrdenado = numeros2
+            //                         .Where(x => x > 4).OrderBy(n => n);
+
+            // foreach (var n in resultadoOrdenado)
+            // {
+            //     Console.WriteLine(n);
+            // }
+
+            // // foreach (var n in numeros.Where(x => x > 4))
+            // // {
+            // //     Console.WriteLine(n);                
+            // // }
+
+            // Console.ReadKey();
+
+            string[] cores = {"Preto", "Branco", "Verde", "Vermelho", "Azul"};
+
+            var resultadoCores = cores.Where(x => x.Contains("e") || x.Contains("z"));
+
+            foreach (var c in resultadoCores)
             {
-                Console.WriteLine(n);                
+                Console.WriteLine(c);
             }
         }
     }
